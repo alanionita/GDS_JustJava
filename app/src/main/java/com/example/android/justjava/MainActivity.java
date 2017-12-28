1705e9d7ca203ae14541075ce260922388494cec
alanionita <a.g.ionita@gmail.com>
2017-12-28 21:42:32 +0000

    adds lower/upper limits to the quantity pickers and Toast functionality when each limit is hit.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,12 +7,14 @@
  **/
 package com.example.android.justjava;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -46,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Calculates the price of the order.
-     *
+     * <p>
      * Makes use of whippedCreamIsChecked and chocolateSauceIsChecked in order to calculate topings variable
      *
      * @return total price, with a price per cup of £5, whippedCream £1, chocolateSauce £2
@@ -69,50 +71,54 @@ public class MainActivity extends AppCompatActivity {
      * @param price
      * @return an order summary String
      */
-    private String createOrderSummary (int price){
+    private String createOrderSummary(int price) {
         return "Name: " + getName()
                 + "\nAdd Whipped Cream? " + whippedCreamIsChecked()
                 + "\nAdd Chocolate Sauce? " + chocolateSauceIsChecked()
                 + "\nQuantity: " + quantity
                 + "\nTotal: £" + price
                 + "\nThank you!";
-    };
+    }
 
     /**
      * Checks the state of the Whipped Cream CheckBox and returns a boolean
      *
      * @return a checked state boolean
      */
-    private Boolean whippedCreamIsChecked () {
+    private Boolean whippedCreamIsChecked() {
         final CheckBox checkBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         return checkBox.isChecked();
-    };
+    }
 
     /**
      * Checks the state of the Chocolate Sauce CheckBox and returns a boolean
      *
      * @return a checked state boolean
      */
-    private Boolean chocolateSauceIsChecked () {
+    private Boolean chocolateSauceIsChecked() {
         final CheckBox checkBox = (CheckBox) findViewById(R.id.chocolate_sauce_checkbox);
         return checkBox.isChecked();
-    };
+    }
 
     /**
      * Gets the text from the Name field
      *
      * @return a string with the name
      */
-    private String getName () {
+    private String getName() {
         final EditText name = (EditText) findViewById(R.id.name_field);
         return name.getText().toString();
-    };
-
+    }
 
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            CharSequence text = "You've reached our maximum limit for coffee orders!";
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -121,6 +127,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            CharSequence text = "You've reached our lower limit for coffee orders!";
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
